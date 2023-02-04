ee021c01a46d7c9d4482e5b32c1ba65f9df4615b
olku11 <artem.chastukhin@gmail.com>
2023-02-04 14:18:03 +0300

    ffg

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<title>')
+def index(title=""):
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
