f0d42f01bbaba1d84a74f3c2fb8b077b1255c0f5
olku11 <artem.chastukhin@gmail.com>
2023-02-21 18:31:14 +0300

    2

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index/<title>')
+@app.route("/")
+@app.route("/index/<title>")
 def index(title=""):
-    return render_template('base.html', title=title)
+    return render_template("base.html", title=title)
 
 
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+@app.route("/training/<prof>")
+def training(prof):
+    pro = prof
+    if "строитель" in pro:
+        pro = "строитель"
+    elif "инженер" in pro:
+        pro = "инженер"
+    return render_template("ex2.html", prof=pro)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
