1dcd29d9f5e7cd0c26939b5b983b22f59a55882a
olku11 <artem.chastukhin@gmail.com>
2023-02-21 20:08:57 +0300

    3

--- main.py
+++ main.py
@@ -20,5 +20,12 @@ def training(prof):
     return render_template("ex2.html", prof=pro)
 
 
+@app.route("/list_prof/<list>")
+def list_prof(list):
+    pr = "Инженер, пилот, строитель, биолог, врач, климатолог, специалист по радиоционной защите, геолог, метеоролог," \
+         "оператор марсохода, штурман, астролог".split(", ")
+    return render_template("ex3.html", list=list, user_list=pr)
+
+
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
